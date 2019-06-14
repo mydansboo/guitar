82ba85f06c0fb265598008470a598fa555438674
Daniel Lewis <daniellewis777@gmail.com>
2019-06-14 16:50:24 +0100

    fixed song title

--- src/app/routes/songs/songs.ts
+++ src/app/routes/songs/songs.ts
@@ -1,7 +1,9 @@
 import hereIAmToWorship from './here-i-am-to-worship.json'
 import isntHeBeautiful from './isnt-he-beautiful.json'
+import goodGoodFather from './good-good-father.json'
 
 export const songs = [
+  hereIAmToWorship,
   isntHeBeautiful,
-  hereIAmToWorship
+  goodGoodFather
 ]
