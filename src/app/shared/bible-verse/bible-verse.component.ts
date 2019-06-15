c43d4e61b131b277a9c68a1178371ed6dffdf576
Daniel Lewis <daniellewis777@gmail.com>
2019-06-15 20:44:28 +0100

    bible verse caching

--- src/app/shared/bible-verse/bible-verse.component.ts
+++ src/app/shared/bible-verse/bible-verse.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { LocalStorageService } from '../../services/local-storage.service'
 
 @Component({
   selector: 'app-bible-verse',
@@ -10,18 +11,32 @@ import { HttpClient } from '@angular/common/http'
 export class BibleVerseComponent implements OnInit {
 
   @Input() bible
-  verse: string
-  ref: string
-  translation: { abbr: string, name: string } = {abbr: null, name: null}
+  verse: {
+    text: string,
+    ref: string,
+    translation: { abbr: string, name: string }
+  }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
 
   ngOnInit() {
-    this.http.get('https://bible-api.com/' + encodeURIComponent(this.bible)).subscribe((res: any) => {
-      this.verse = res.text
-      this.ref = res.reference.toUpperCase()
-      this.translation.abbr = res.translation_id.toUpperCase()
-      this.translation.name = res.translation_name
-    })
+
+    const bibleKey = 'bible-' + this.bible.toLowerCase().replace(' ', '')
+
+    this.verse = this.localStorageService.getItem(bibleKey)
+
+    if (!this.verse) {
+      this.http.get('https://bible-api.com/' + encodeURIComponent(this.bible)).subscribe((res: any) => {
+        this.verse = {
+          text: res.text,
+          ref: res.reference.toUpperCase(),
+          translation: {
+            abbr: res.translation_id.toUpperCase(),
+            name: res.translation_name
+          }
+        }
+        this.localStorageService.setItem(bibleKey, this.verse)
+      })
+    }
   }
 }
