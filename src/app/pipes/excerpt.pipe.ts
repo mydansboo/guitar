fde6f16b0fd9a788c09ce56ba0ee6e98afb055be
Daniel Lewis <daniellewis777@gmail.com>
2019-06-15 20:02:44 +0100

    fixed excerpt

--- src/app/pipes/excerpt.pipe.ts
+++ src/app/pipes/excerpt.pipe.ts
@@ -10,8 +10,13 @@ export class ExcerptPipe implements PipeTransform {
     }
 
     const temp = str.split(',')[0].split('.')[0]
-    const idx = getPosition(temp, ' ', 5)
+    const idx1 = getPosition(temp, ' ', 5)
+    const idx2 = getPosition(temp, ' ', 6)
+    const idx3 = getPosition(temp, ' ', 7)
+    const str1 = temp.substring(0, idx1)
+    const str2 = temp.substring(0, idx2)
+    const str3 = temp.substring(0, idx3)
 
-    return temp.substring(0, idx)
+    return (str1.split('-').length === str2.split('-').length) ? str1 : str3
   }
 }
