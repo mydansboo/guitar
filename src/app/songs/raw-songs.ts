32b1bf896d430a69780a768bf1b5e59f1fb20950
Daniel Lewis <daniellewis777@gmail.com>
2019-06-23 13:52:20 +0100

    A great change

--- src/app/songs/raw-songs.ts
+++ src/app/songs/raw-songs.ts
@@ -1,8 +1,10 @@
 import hereIAmToWorship from './here-i-am-to-worship.json'
 import isntHeBeautiful from './isnt-he-beautiful.json'
 import goodGoodFather from './good-good-father.json'
+import iAmAChristian from './i-am-a-christian.json'
 
 export const rawSongs = [
+  iAmAChristian,
   hereIAmToWorship,
   isntHeBeautiful,
   goodGoodFather
