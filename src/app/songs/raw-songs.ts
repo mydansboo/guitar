368590e7b2bf7a1d3b7f448b24d048656e83c867
Daniel Lewis <daniellewis777@gmail.com>
2019-06-23 20:08:09 +0100

    save

--- src/app/songs/raw-songs.ts
+++ src/app/songs/raw-songs.ts
@@ -1,11 +1,9 @@
 import hereIAmToWorship from './here-i-am-to-worship.json'
 import isntHeBeautiful from './isnt-he-beautiful.json'
-import goodGoodFather from './good-good-father.json'
 import iAmAChristian from './i-am-a-christian.json'
 
 export const rawSongs = [
   iAmAChristian,
   hereIAmToWorship,
-  isntHeBeautiful,
-  goodGoodFather
+  isntHeBeautiful
 ]
