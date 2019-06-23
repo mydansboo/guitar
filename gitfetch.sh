9cbb88abb5ef312e724679ae01087eb30993d0fc
Daniel Lewis <daniellewis777@gmail.com>
2019-06-23 03:41:41 +0100

    gitfetch

--- gitfetch.sh
+++ gitfetch.sh
@@ -3,7 +3,8 @@
 # On Ubuntu 18 Bionic Beaver
 # crontab -e  edit cron jobs
 # crontab -l  view cron jobs
-# * * * * * /root/guitar/gitfetch
+# * * * * * /root/guitar/gitfetch.sh
+# * * * * * env > /root/guitar/cronticker
 
 cd /root/guitar
 git fetch -p
