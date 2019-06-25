8ce017e888dd323896aebab56fb75c2d19b61c94
Daniel Lewis <daniellewis777@gmail.com>
2019-06-25 20:16:47 +0100

    bless the Lord

--- src/app/songs/raw-songs.ts
+++ src/app/songs/raw-songs.ts
@@ -1,8 +1,10 @@
 import hereIAmToWorship from './here-i-am-to-worship.json'
 import isntHeBeautiful from './isnt-he-beautiful.json'
 import iAmAChristian from './i-am-a-christian.json'
+import blessTheLordOMySoul from './bless-the-lord-o-my-soul.json'
 
 export const rawSongs = [
+  blessTheLordOMySoul,
   iAmAChristian,
   hereIAmToWorship,
   isntHeBeautiful
